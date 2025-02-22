9919106f8b49d005c3174b02afef291fcdd3e5d5
umitphy <umitakdenizjob@gmail.com>
2025-02-22 22:48:34 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. anything else? wtg
+          Edit <code>src/App.js</code> and save to reload. anything else? wtg. Yıl adlı bunu yaomak.
         </p>
         <a
           className="App-link"
